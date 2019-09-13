88544d5f0b93234e184e49ec7280804dfc1d001c
ethArek <arkadiusz.sidzinka@gmail.com>
2019-09-13 16:32:32 +0200

    add login route

--- API/users/index.js
+++ API/users/index.js
@@ -5,13 +5,42 @@ const { User } = require("../../models/user.js");
 router.post("/register", async (req, res) => {
   try {
     const body = _.pick(req.body, ["email", "password"]);
-    const user = new User(body);
+    if (!body.password || !body.email) {
+      res
+        .status(422)
+        .json({ success: false, message: "Pola e-mail i hasło są wymagane" });
+    } else {
+      const user = new User(body);
 
-    await user.save();
-    res.json({ success: true, message: "Dodano uzytkownika" });
+      await user.save();
+      res.json({ success: true, message: "Dodano uzytkownika" });
+    }
   } catch (err) {
     res
       .status(500)
       .json({ success: false, message: "Błąd połączenia z serwerem" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const body = _.pick(req.body, ["email", "password"]);
+    if (!body.password || !body.email) {
+      res
+        .status(422)
+        .json({ success: false, message: "Pola e-mail i hasło są wymagane" });
+    } else {
+      const user = await User.findByCredentials(body.email, body.password);
+      const token = await user.generateAuthToken();
+      const result = {
+        userId: user._id,
+        token: token,
+        email: user.email
+      };
+      res.json({ success: true, result });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ success: false, error: err });
   }
 });
