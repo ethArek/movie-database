88345030e3340f9ba0768b079a426576359f98db
ethArek <arkadiusz.sidzinka@gmail.com>
2019-09-17 21:55:13 +0200

    update POST /movies, add moment.js

--- API/comments/index.js
+++ API/comments/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.send("asfasdf");
+  res.send("");
 });
 
 module.exports = router;
--- API/movies/index.js
+++ API/movies/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const request = require("request-promise");
+const moment = require("moment");
 
 const { omdbApi } = require("../../config/config.js");
 const Movie = require("../../models/movies.js");
 const auth = require("../../middlewares/authentication.js");
+// require("../../db/connection.js");
 
-router.post("/", auth, async (req, res) => {
+router.post("/", async (req, res) => {
   if (req.body.title || req.body.imdbId) {
     try {
       const requestOptions = {
@@ -19,9 +21,38 @@ router.post("/", auth, async (req, res) => {
         },
         json: true
       };
-
+      console.log("here");
       const response = await request(requestOptions);
-      res.send(response);
+      if (response.Response === "True") {
+        const body = {
+          title: response.Title,
+          year: response.Year,
+          rating: response.Rated,
+          releaseDate: response.releaseDate
+            ? moment(response.releaseDate, "DD MMM YYYY").toDate()
+            : null,
+          plot: response.Plot,
+          actors: response.Actors,
+          duration: response.Runtime,
+          genre: response.Genre,
+          imdbRating: response.imdbRating,
+          boxOffice: response.BoxOffice,
+          posterImage: response.Poster !== "N/A" ? response.Poster : null
+        };
+
+        const movie = new Movie(body);
+        await movie.save();
+        res.json({
+          success: true,
+          message: "Film został dodany do bazy",
+          movie_id: movie._id
+        });
+      } else {
+        res.status(422).json({
+          success: false,
+          message: "Niestety nie znaleziono filmu o podanych kryteriach"
+        });
+      }
     } catch (err) {
       res.status(500).json({ success: false, message: "Problemy z serwerem" });
     }
