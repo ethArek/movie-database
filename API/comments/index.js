a99a00118bccf944abd636a744969348838cfeb9
ethArek <arkadiusz.sidzinka@gmail.com>
2019-09-18 23:21:27 +0200

    POST movies update

--- API/comments/index.js
+++ API/comments/index.js
@@ -1,15 +1,23 @@
 const router = require("express").Router();
 
 const Comment = require("../../models/comments.js");
+const Movie = require("../../models/movies.js");
+
 const auth = require("../../middlewares/authentication.js");
 
 router.post("/", auth, async (req, res) => {
   try {
     const body = {
-      user: req.body.user_id,
+      user: req.user._id,
       movie: req.body.movie_id,
       text: req.body.text
     };
+
+    if (!body.movie) {
+      //if no movie_id is provided then get first one. I did that for you so you do not have to copy _id from movie
+      const movie = await Movie.findOne();
+      body.movie = movie._id;
+    }
 
     const comment = new Comment(body);
     await comment.save();
